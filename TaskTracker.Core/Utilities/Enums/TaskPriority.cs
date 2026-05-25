6d4239b6bfd2441a80ee14e7d306c8ee3db68a9b
Can Engin Cizmeci <canncizmeci@gmail.com>
2026-05-25 23:11:06 +0300

    feat(api): add task status and priority enums

---
+++ TaskTracker.Core/Utilities/Enums/TaskPriority.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTracker.Core.Utilities.Enums
+{
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
