96f6df6d0856e8930388ca418b2bea78c7888fac
Can Engin Cizmeci <canncizmeci@gmail.com>
2026-05-27 15:15:59 +0300

    refactor(data): rename EfTaskTrackerDal to EfTaskRequestDal

---
+++ TaskTracker.DataAccess/Concrete/EfCore/EfTaskRequestDal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTracker.Core.DataAccess;
+using TaskTracker.Core.DataAccess.EfCore.Repository;
+using TaskTracker.Core.Entities.Concrete;
+using TaskTracker.DataAccess.Abstract;
+
+namespace TaskTracker.DataAccess.Concrete.EfCore
+{
+    public class EfTaskRequestDal : EfEntityRepositoryBase<TaskRequest, TaskTrackerDbContext>, ITaskRequestDal
+    {
+        public EfTaskRequestDal(TaskTrackerDbContext context) : base(context)
+        {
+            
+        }
+        public Task<bool> CanEditAsync(int taskId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CanManageAsync(int taskId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CanViewAsync(int taskId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
