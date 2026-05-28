507604fdddd83ca0e6ad517ae41c9e7f5247d3b6
Can Engin Cizmeci <canncizmeci@gmail.com>
2026-05-28 22:20:00 +0300

    feat(task-sharing): add invitation payload model for shared task access

---
+++ TaskTracker.Entities/DTOs/InviteUserToTaskDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTracker.Core.Entities;
+using TaskTracker.Core.Utilities.Enums;
+
+namespace TaskTracker.Entities.DTOs
+{
+    public class InviteUserToTaskDto:IDto
+    {
+        public int TaskRequestId { get; set; }
+        public string Username { get; set; }
+        public TaskPermission Permission { get; set; } 
+        public DateTime? SharedAt { get; set; }
+    }
+}
