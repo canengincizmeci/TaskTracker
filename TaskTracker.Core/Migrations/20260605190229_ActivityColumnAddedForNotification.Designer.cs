d9def71dab7613759db77f8d9ca28312213dff66
Can Engin Cizmeci <canncizmeci@gmail.com>
2026-06-05 22:04:02 +0300

    feat(task-sharing) added activity column for notification table with migration

--- TaskTracker.Core/Migrations/20260605190229_ActivityColumnAddedForNotification.Designer.cs
+++ TaskTracker.Core/Migrations/20260605190229_ActivityColumnAddedForNotification.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskTracker.Core.DataAccess;
@@ -11,9 +12,11 @@ using TaskTracker.Core.DataAccess;
 namespace TaskTracker.Core.Migrations
 {
     [DbContext(typeof(TaskTrackerDbContext))]
-    partial class TaskTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260605190229_ActivityColumnAddedForNotification")]
+    partial class ActivityColumnAddedForNotification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TaskTracker.Core/Migrations/20260605190229_ActivityColumnAddedForNotification.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivityColumnAddedForNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activity",
+                table: "Notifications",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activity",
+                table: "Notifications");
+        }
+    }
+}
