4707497feb11a481452714a15ea74cff9386b3bc
Can Engin Cizmeci <canncizmeci@gmail.com>
2026-08-30 16:27:27 +0300

    feat: add SignalR notification manager

---
+++ TaskTracker.API/Services/SignalRNotificationManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TaskTracker.API.Hubs;
+using TaskTracker.Bussiness.Abstract;
+
+namespace TaskTracker.API.Services
+{
+    public class SignalRNotificationManager : IRealtimeNotificationService
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public SignalRNotificationManager(
+            IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        public async Task SendNotificationAsync(int userId, string message)
+        {
+            await _hubContext.Clients.User(userId.ToString()).SendAsync("ReceiveNotification", message);
+        }
+    }
+}
