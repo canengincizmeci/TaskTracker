fd9b43ec68004690a08dd1ca2d5c11abbb39893f
Can Engin Cizmeci <canncizmeci@gmail.com>
2026-08-30 16:15:07 +0300

    feat: add realtime notification service abstraction

---
+++ TaskTracker.Bussiness/Abstract/IRealtimeNotificationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTracker.Bussiness.Abstract
+{
+    public interface IRealtimeNotificationService
+    {
+        Task SendNotificationAsync(int userId, string message);
+    }
+}
